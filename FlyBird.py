94eea9a1953d674013171d4f752ab735dd24df6b
PROG-007 <raghavadi007@gmail.com>
2024-09-26 19:19:14 +0530

    insomnia++

--- FlyBird.py
+++ FlyBird.py
@@ -2,7 +2,6 @@ import pygame
 import sys
 import random
 
-# Game Variables
 gravity = 0.25
 bird_movement = 0
 game_active = True
@@ -10,35 +9,28 @@ score = 0
 high_score = 0
 pipe_speed = 3
 
-# Pygame Setup
 pygame.init()
 screen_width = 1020
 screen_height = 512
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
-# Colors
 bg_color = (135, 206, 235)
 floor_color = (225, 215, 188)
 
-# Fonts
 font = pygame.font.Font('freesansbold.ttf', 50)
 
-# Background
 bg_surface = pygame.Surface((screen_width, screen_height))
 bg_surface.fill(bg_color)
 
-# Floor
 floor_surface = pygame.Surface((screen_width, 112))
 floor_surface.fill(floor_color)
 floor_x_pos = 0
 
-# Bird
 bird_surface = pygame.Surface((20, 20), pygame.SRCALPHA)
 bird_surface.fill((255, 255, 255))
 bird_rect = bird_surface.get_rect(center=(100, 256))
 
-# Pipes
 pipe_surface = pygame.Surface((50, 600))
 pipe_surface.fill((50, 205, 50))
 pipe_list = []
@@ -97,25 +89,21 @@ while True:
     screen.blit(bg_surface, (0, 0))
 
     if game_active:
-        # Bird
         bird_movement += gravity
         bird_rect.centery += bird_movement
         rotated_bird = rotate_bird(bird_surface)
         screen.blit(rotated_bird, bird_rect)
         game_active = check_collision(pipe_list)
 
-        # Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
 
-        # Score and Speed Increment
         score += 0.01
-        if score % 10 == 0:  # Increase pipe speed every 10 points
+        if score % 10 == 0:  
             pipe_speed += 0.01
         score_display = font.render(str(int(score)), True, (255, 255, 255))
         screen.blit(score_display, (144, 50))
 
-        # Remove off-screen pipes
         if pipe_list and pipe_list[0].right < 0:
             pipe_list.pop(0)
             pipe_list.pop(0)
@@ -131,7 +119,7 @@ while True:
         screen.blit(high_score_display, (60, 200))
 
     # Floor with Parallax
-    floor_x_pos -= 4  # Move floor faster than pipes for parallax effect
+    floor_x_pos -= 4 
     screen.blit(floor_surface, (floor_x_pos, 400))
     screen.blit(floor_surface, (floor_x_pos + screen_width, 400))
     if floor_x_pos <= -screen_width:
