f0cdd683e5bd309d71bc8017c79ca54f864e61e4
PROG-007 <raghavadi007@gmail.com>
2024-09-26 19:15:53 +0530

    insomnia

--- FlyBird.py
+++ FlyBird.py
@@ -8,6 +8,7 @@ bird_movement = 0
 game_active = True
 score = 0
 high_score = 0
+pipe_speed = 3
 
 # Pygame Setup
 pygame.init()
@@ -33,7 +34,7 @@ floor_surface.fill(floor_color)
 floor_x_pos = 0
 
 # Bird
-bird_surface = pygame.Surface((20, 20))
+bird_surface = pygame.Surface((20, 20), pygame.SRCALPHA)
 bird_surface.fill((255, 255, 255))
 bird_rect = bird_surface.get_rect(center=(100, 256))
 
@@ -42,18 +43,18 @@ pipe_surface = pygame.Surface((50, 600))
 pipe_surface.fill((50, 205, 50))
 pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
-pygame.time.set_timer(SPAWNPIPE, 800)  # Reduced time to spawn pipes
+pygame.time.set_timer(SPAWNPIPE, 1200)  # Adjusted time to spawn pipes
 pipe_height = [200, 300, 400]
 
 def create_pipe():
     random_pipe = random.choice(pipe_height)
-    bottom_pipe = pipe_surface.get_rect(midtop=(300, random_pipe))
-    top_pipe = pipe_surface.get_rect(midbottom=(300, random_pipe - 150))
+    bottom_pipe = pipe_surface.get_rect(midtop=(1020, random_pipe))
+    top_pipe = pipe_surface.get_rect(midbottom=(1020, random_pipe - 150))
     return bottom_pipe, top_pipe
 
 def move_pipes(pipes):
     for pipe in pipes:
-        pipe.centerx -= 5
+        pipe.centerx -= pipe_speed
     return pipes
 
 def draw_pipes(pipes):
@@ -70,6 +71,10 @@ def check_collision(pipes):
 
     return True
 
+def rotate_bird(bird_surface):
+    new_bird = pygame.transform.rotozoom(bird_surface, -bird_movement * 3, 1)
+    return new_bird
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -85,10 +90,9 @@ while True:
                 bird_rect.center = (100, 256)
                 bird_movement = 0
                 score = 0
+                pipe_speed = 3  # Reset pipe speed
         if event.type == SPAWNPIPE:
-            bottom_pipe, top_pipe = create_pipe()
-            pipe_list.append(bottom_pipe)
-            pipe_list.append(top_pipe)
+            pipe_list.extend(create_pipe())
 
     screen.blit(bg_surface, (0, 0))
 
@@ -96,25 +100,27 @@ while True:
         # Bird
         bird_movement += gravity
         bird_rect.centery += bird_movement
-        screen.blit(bird_surface, bird_rect)
+        rotated_bird = rotate_bird(bird_surface)
+        screen.blit(rotated_bird, bird_rect)
         game_active = check_collision(pipe_list)
 
         # Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
 
-        # Score
+        # Score and Speed Increment
         score += 0.01
+        if score % 10 == 0:  # Increase pipe speed every 10 points
+            pipe_speed += 0.01
         score_display = font.render(str(int(score)), True, (255, 255, 255))
         screen.blit(score_display, (144, 50))
 
         # Remove off-screen pipes
-        if pipe_list:
-            if pipe_list[0].right < 0:
-                pipe_list.pop(0)
-                pipe_list.pop(0)
+        if pipe_list and pipe_list[0].right < 0:
+            pipe_list.pop(0)
+            pipe_list.pop(0)
 
-    if game_active == False:
+    else:
         game_over_display = font.render('Game Over', True, (255, 255, 255))
         screen.blit(game_over_display, (75, 100))
         score_display = font.render(f'Score: {int(score)}', True, (255, 255, 255))
@@ -124,8 +130,8 @@ while True:
         high_score_display = font.render(f'High Score: {int(high_score)}', True, (255, 255, 255))
         screen.blit(high_score_display, (60, 200))
 
-    # Floor
-    floor_x_pos -= 1
+    # Floor with Parallax
+    floor_x_pos -= 4  # Move floor faster than pipes for parallax effect
     screen.blit(floor_surface, (floor_x_pos, 400))
     screen.blit(floor_surface, (floor_x_pos + screen_width, 400))
     if floor_x_pos <= -screen_width:
